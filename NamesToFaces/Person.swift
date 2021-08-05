622c14b5d40cf25523a9e3cb5ecb1eed31d661e8
markojovanov <markojovanov999@gmail.com>
2021-08-05 20:32:45 +0200

    Update v1.0

---
+++ NamesToFaces/Person.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
